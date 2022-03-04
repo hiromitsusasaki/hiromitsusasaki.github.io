621070cdd6b676bc4478e38ae14c01d3a02c7962
hiromitsusasaki <hiromitsu.sasaki@gmail.com>
2022-03-04 16:56:47 +0900

    feat: Apply chakraui to next

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from './navbar'
 import Footer from './footer'
 import Header from './header'
@@ -5,10 +6,12 @@ import Header from './header'
 export default function Layout({ children }: any) {
     return (
         <>
-            <Header />
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
+            <ChakraProvider>
+                <Header />
+                <Navbar />
+                <main>{children}</main>
+                <Footer />
+            </ChakraProvider>
         </>
     )
 }
